944d7c53bf6ab5453631ee072d61d86fa5cc98cd
Ch. Theodorou <theodoroucharalabos@gmail.com>
2018-01-09 00:16:54 +0200

    Create HappyFortuneService.java

---
+++ HappyFortuneService.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+public class HappyFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return "Its your lucky day";
+	}
+
+	
+
+}
